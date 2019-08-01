53a7e385b2f998d00decfd6a983930a394dcb043
Alex Velasco <alex@mi-debian>
2019-08-01 08:26:53 +0200

    [FEAT] resume: Add learning technologies section & pick angular dependences in local

--- app/scripts/controllers/resume.js
+++ app/scripts/controllers/resume.js
@@ -6,14 +6,17 @@ angular.module( 'appApp' )
     this.skill = "";
     this.skillType = "";
     this.aConduct = [ 'Teamworking skills', 'Good interpersonal skills', 'Efective Communication', 'Curiosity', 'Scrum', 'Kanban' ];
-    this.aSkills = [ 'AngularJS', 'Jenkins', 'Ansible', 'Docker', 'Git', 'Debian OS' ];
+    this.aSkills = [ 'Docker', 'Jenkins', 'Ansible', 'Docker Swarm', 'Git', 'Debian OS' ];
     //Programming Languages
     this.aPL = [ "C", "C++", "Python", "Node.js", "Java EE", "Bash Scriping" ];
+    this.aLT = [ "Maven", "SonarQube", "Terraform", "Kubernetes" ];
     this.aOtherSkills = [ ];
     this.displayAll = false;
     this.changeSkill = ( ) => {
-        var allElements = this.aConduct.concat( this.aSkills )
-        .concat( this.aPL );
+        var allElements = this.aConduct
+        .concat( this.aSkills )
+        .concat( this.aPL )
+        .concat( this.aLT );
         //to check another skill
         var element;
         do {
@@ -27,6 +30,7 @@ angular.module( 'appApp' )
 
         if ( this.aPL.indexOf( this.skill ) !== -1 ) this.skillType = "Programming Language";
 
+        if ( this.aLT.indexOf( this.skill ) !== -1 ) this.skillType = "Learning Technology";
     }
     this.addOtherSkill = function ( name_, type_ ) {
         this.aOtherSkills.push( {
